2881f8f5e6ea0d86ef1c4ca77f9cb279a34c2d19
SamPam972 <samuelpame@outlook.com>
2022-07-26 17:30:38 +0200

    Update f_bdd.php

--- include/function/f_bdd.php
+++ include/function/f_bdd.php
@@ -4,7 +4,7 @@ function connexionBDD()
 {
 	try
 	{
-            $con=mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "geststagesdb-mi-pame.mariadb.database.azure.com", "admindb@geststagesdb-mi-pame", "MIDBadmin,92" , "bdd_geststages", 3306);
+            $con= mysqli_init(); mysqli_ssl_set($con, NULL, NULL, {ca-cert filename}, NULL, NULL); mysqli_real_connect($con, "geststagesdb-mi-pame.mariadb.database.azure.com", "admindb@geststagesdb-mi-pame", "MIDBadmin,92" , "bdd_geststages", 3306);
             return $con;
 	}
 	catch(Exception $e)
