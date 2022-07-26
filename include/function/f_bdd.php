3dae8dca8661a209ce388c165fcb2b1a436d7198
SamPam972 <samuelpame@outlook.com>
2022-07-26 16:56:29 +0200

    Update f_bdd.php

--- include/function/f_bdd.php
+++ include/function/f_bdd.php
@@ -4,7 +4,7 @@ function connexionBDD()
 {
 	try
 	{
-            $bdd = $con= mysqli_init(); mysqli_real_connect($con, "geststagesdb-mi-pame.mariadb.database.azure.com", "admindb@geststagesdb-mi-pame", "MIDBadmin,972", "bdd_geststages", 3306);
+            $bdd = $con= new PDO('mysql:host=geststagesdb-mi-pame.mariadb.database.azure.com;port=3306;dbname=bdd_geststages;charset=utf8', 'admindb@geststagesdb-mi-pame', 'MIDBadmin,972', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
             return $bdd;
 	}
 	catch(Exception $e)
